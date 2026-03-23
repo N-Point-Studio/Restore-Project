c50e411e94f1d16b607fa600e0f0a39ffc519ab2
Key <shikirie@gmail.com>
2026-03-24 01:14:58 +0700

    Ketinggalan

--- Assets/[APP]/Scripts/MainMenu/Controller/SettingsController.cs
+++ Assets/[APP]/Scripts/MainMenu/Controller/SettingsController.cs
@@ -1,8 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using VContainer;
 
 public class SettingsController : BaseMenuController
 {
+    [SerializeField] private ButtonInputInstructionUI backButton;
+    [SerializeField] private Slider masterSoundSlider;
+    [SerializeField] private Slider musicSoundSlider;
+    [SerializeField] private Slider sfxSoundSlider;
+    [SerializeField] private Toggle crossHairToggle;
+
     private ActiveSettingsData activeSettingsData;
     
     [Inject]
@@ -10,5 +18,22 @@ public class SettingsController : BaseMenuController
         ActiveSettingsData activeSettingsData)
     {
         this.activeSettingsData = activeSettingsData;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        backButton.OnClick += OnBackButtonClick;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        backButton.OnClick -= OnBackButtonClick;
+    }
+
+    private void OnBackButtonClick()
+    {
+        SetActive(false);
     }
 }
