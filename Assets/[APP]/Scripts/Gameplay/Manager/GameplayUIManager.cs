878c664419d13e292138005c86b03b16c815eafe
Fatehehe <fatakhillah.khaqo@gmail.com>
2026-03-30 22:39:02 +0700

    Fix: Tutorial bug fixed

--- Assets/[APP]/Scripts/Gameplay/Manager/GameplayUIManager.cs
+++ Assets/[APP]/Scripts/Gameplay/Manager/GameplayUIManager.cs
@@ -203,7 +203,7 @@ public class GameplayUIManager : MonoBehaviour
 
         if (tutorialService.CurrentStage == 0 && tutorialService.CurrentModule == 4)
         {
-            tutorialService.CompleteAndAdvance();
+            tutorialService.CompleteStage();
         }
     }
 
@@ -255,7 +255,7 @@ public class GameplayUIManager : MonoBehaviour
 
         if (tutorialService.CurrentStage == 1 && tutorialService.CurrentModule == 0)
         {
-            tutorialService.CompleteAndAdvance();
+            tutorialService.CompleteStage();
         }
     }
 
